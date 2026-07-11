c9820b156504a83be48604f47078f924d0004e5e
Алла <alla@MacBook-Air-Alla.local>
2026-07-11 16:53:57 +0700

    move widget to src and add date function

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from masks.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(info: str) -> str:
+    parts = info.split()
+
+    if len(parts) < 2:
+        return "Некорректный ввод"
+
+    number = parts[-1]
+    name = " ".join(parts[:-1])
+
+    if name.lower().startswith("счет"):
+        return f"{name} {get_mask_account(number)}"
+
+    return f"{name} {get_mask_card_number(number)}"
+
+
+def get_date(date_string: str) -> str:
+    date = datetime.fromisoformat(date_string)
+    return date.strftime("%d.%m.%Y")
