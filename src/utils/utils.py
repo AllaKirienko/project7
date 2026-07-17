940d043606f0172b3f5f4a3189da15a52318f2ba
Алла <alla@MacBook-Air-Alla.local>
2026-07-17 17:05:39 +0700

    Add JSON reading and currency conversion

---
+++ src/utils/utils.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+
+
+def read_json(file_path: str) -> list[dict]:
+    """Читает JSON-файл и возвращает список словарей с данными."""
+
+    try:
+        with Path(file_path).open("r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        if isinstance(data, list):
+            return data
+
+        return []
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
