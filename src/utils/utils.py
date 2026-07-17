2cedc43fc3b6f7e66880f3ee1738a569efe5bbfe
Алла <alla@MacBook-Air-Alla.local>
2026-07-17 22:21:21 +0700

    Add logging for masks and utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,26 @@
 import json
+import logging
 from pathlib import Path
+
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler(
+    "logs/utils.log",
+    mode="w",
+    encoding="utf-8"
+)
+
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
 
 def read_json_file(file_path: str) -> list[dict]:
     """Читает JSON-файл и возвращает список словарей с данными."""
@@ -9,9 +30,16 @@ def read_json_file(file_path: str) -> list[dict]:
             data = json.load(file)
 
         if isinstance(data, list):
+            logger.debug("JSON файл успешно прочитан")
             return data
 
+        logger.error("JSON файл содержит не список")
         return []
 
-    except (FileNotFoundError, json.JSONDecodeError):
+    except FileNotFoundError:
+        logger.error("JSON файл не найден")
+        return []
+
+    except json.JSONDecodeError:
+        logger.error("Ошибка чтения JSON файла")
         return []
